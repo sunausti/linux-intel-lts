d487d1b7fcec180178683e5b95c2ff9e2ef872d5
imititel <ionel-catalin.mititelu@intel.com>
2024-03-21 14:15:40 +0000

    net-phy-add-registers-to-support-1000BASE-T1

--- include/uapi/linux/mdio.h
+++ include/uapi/linux/mdio.h
@@ -81,6 +81,26 @@
 #define MDIO_AN_T1_LP_H		519	/* BASE-T1 AN LP Base Page ability register [47:32] */
 #define MDIO_PMA_PMD_BT1_CTRL	2100	/* BASE-T1 PMA/PMD control register */
 
+#define MDIO_PCS_1000BT1_CTRL  2304    /* 1000BASE-T1 PCS control register */
+#define MDIO_PCS_1000BT1_STAT  2305    /* 1000BASE-T1 PCS status register */
+
+/* BASE-T1 PMA/PMD extended ability register. */
+#define MDIO_PMA_PMD_BT1_B100_ABLE     0x0001  /* 100BASE-T1 Ability */
+#define MDIO_PMA_PMD_BT1_B1000_ABLE    0x0002  /* 1000BASE-T1 Ability */
+
+#define MDIO_PMA_PMD_BT1_CTRL_STRAP            0x000F /* Type selection (Strap) */
+#define MDIO_PMA_PMD_BT1_CTRL_STRAP_B1000      0x0001 /* Select 1000BASE-T1 */
+#define MDIO_PMA_PMD_BT1_CTRL_CFG_MST          0x4000 /* MASTER-SLAVE config value */
+
+/* 1000BASE-T1 PCS control register */
+#define MDIO_PCS_1000BT1_CTRL_LOW_POWER                0x0800 /* Low power mode */
+#define MDIO_PCS_1000BT1_CTRL_DISABLE_TX       0x4000 /* Global PMA transmit disable */
+#define MDIO_PCS_1000BT1_CTRL_RESET            0x8000 /* Software reset value */
+
+/* 1000BASE-T1 PCS status register */
+#define MDIO_PCS_1000BT1_STAT_LINK     0x0004 /* PCS Link is up */
+#define MDIO_PCS_1000BT1_STAT_FAULT    0x0080 /* There is a fault condition */
+
 /* LASI (Link Alarm Status Interrupt) registers, defined by XENPAK MSA. */
 #define MDIO_PMA_LASI_RXCTRL	0x9000	/* RX_ALARM control */
 #define MDIO_PMA_LASI_TXCTRL	0x9001	/* TX_ALARM control */
