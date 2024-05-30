2ab38aaee2fd2db18d26ef48b580ca1170e5eedd
imititel <ionel-catalin.mititelu@intel.com>
2024-05-30 13:03:09 +0300

    net-phy-c45-genphy_c45_ethtool_set_eee-validate-EEE-link-modes

--- drivers/net/phy/phy-c45.c
+++ drivers/net/phy/phy-c45.c
@@ -1405,11 +1405,21 @@ int genphy_c45_ethtool_set_eee(struct phy_device *phydev,
 
        if (data->eee_enabled) {
 	       if (data->advertised) {
+		        __ETHTOOL_DECLARE_LINK_MODE_MASK(adv);
+
+                        ethtool_convert_legacy_u32_to_link_mode(adv,
+                                                               data->advertised);
+                        linkmode_andnot(adv, adv, phydev->supported_eee);
+                        if (!linkmode_empty(adv)) {
+                               phydev_warn(phydev, "At least some EEE link modes are not supported.\n");
+                               return -EINVAL;
+                        }
 			ethtool_convert_legacy_u32_to_link_mode(phydev->advertising_eee,
                                                                 data->advertised);
-               else
+	       } else {
                        linkmode_copy(phydev->advertising_eee,
                                      phydev->supported_eee);
+	       }
 
                phydev->eee_enabled = true;
        } else {
